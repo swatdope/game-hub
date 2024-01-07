9a4dc96ee4f3b51f6595fd018140438eb5fd7c7e
Tenzin Ngudup <tenzinngodup900@gmail.com>
2024-01-07 20:25:10 +0530

    Final modification

--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -9,3 +9,15 @@ interface Platform {
 const usePlatform = () => useData<Platform>('/platforms/lists/parents')
 
 export default usePlatform 
+
+// import platforms from "../data/platforms";
+
+// interface Platform {
+//   id: number;
+//   name: string;
+//   slug: string;
+// }
+
+// const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+
+// export default usePlatforms;
