36c47e074490055a9fbaa16f5ffaba32e7d31f65
Tenzin Ngudup <tenzinngodup900@gmail.com>
2024-01-07 17:04:39 +0530

    Handle games with no image

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,6 +5,7 @@ import getCroppedImageUrl from "../services/image-url"
 interface Props {
     onSelectGenre: (genre: Genre) => void;
     selectedGenre: Genre | null;
+    
 }
 
 const GenreList = ({onSelectGenre, selectedGenre}: Props) => {
