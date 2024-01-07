d4f5d319bd9d435b6217ada62ffb206ae8b42304
Tenzin Ngudup <tenzinngodup900@gmail.com>
2024-01-07 19:56:44 +0530

    Refactor: game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,9 +15,9 @@ const GameGrid = ({gameQuery}: Props) => {
 
   const skeletons = [1, 2, 3, 4, 5, 6];
 
+  if(error) return <Text>{error}</Text>
+
   return (
-    <> 
-      {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         spacing={6} 
@@ -35,7 +35,6 @@ const GameGrid = ({gameQuery}: Props) => {
           </GameCardContainer>
         ))}
       </SimpleGrid>
-    </>
   );
 };
 
