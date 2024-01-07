cc671d9bf2ec0e3615ef438d6597518b1a0692c9
Tenzin Ngudup <tenzinngodup900@gmail.com>
2024-01-07 14:51:34 +0530

    Highlight the selected genre

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App = () => {
         <Navbar />
      </GridItem>
       <Show above="lg" >
-      <GridItem area="aside" paddingX={5} > <GenreList onSelectGenre={(genre) => setSelectedGenre(genre)} /> </GridItem>
+      <GridItem area="aside" paddingX={5} > <GenreList selectedGenre={selectedGenre } onSelectGenre={(genre) => setSelectedGenre(genre)} /> </GridItem>
       </Show>
       <GridItem area="main" >
         <GameGrid selectedGenre={selectedGenre} />
