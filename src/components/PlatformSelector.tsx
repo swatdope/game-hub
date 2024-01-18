82c79a178ae4fce670d4bb88418cf9590c3b4196
Tenzin Ngudup <tenzinngodup900@gmail.com>
2024-01-18 19:12:41 +0530

    Fetching platform

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,32 +1,34 @@
-import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react"
-import { BsChevronBarDown } from "react-icons/bs"
-import usePlatform from "../hooks/usePlatform"
-import { Platform } from "../hooks/useGames"
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { BsChevronBarDown } from "react-icons/bs";
+import usePlatform from "../hooks/usePlatform";
+import { Platform } from "../hooks/useGames";
 
 interface Props {
-    onSelectPlatform: (platform: Platform ) => void;
-    selectedPlatform: Platform | null;
+  onSelectPlatform: (platform: Platform) => void;
+  selectedPlatform: Platform | null;
 }
- 
 
-const PlatformSelector = ({onSelectPlatform, selectedPlatform}: Props) => {
-    const {data,error} =  usePlatform() 
+const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
+  const { data, error } = usePlatform();
 
-    if(error) return null
+  if (error) return null;
   return (
     <Menu>
-        <MenuButton as={Button} rightIcon={<BsChevronBarDown />} >
-            {selectedPlatform?.name || 'Platfroms'}
-        </MenuButton>
-        <MenuList>
-            {data.map(platform => 
-                <MenuItem onClick={() => onSelectPlatform(platform)} key={platform.id} >
-                    {platform.name}
-                </MenuItem>
-            )}  
-        </MenuList>
+      <MenuButton as={Button} rightIcon={<BsChevronBarDown />}>
+        {selectedPlatform?.name || "Platfroms"}
+      </MenuButton>
+      <MenuList>
+        {data?.results.map((platform) => (
+          <MenuItem
+            onClick={() => onSelectPlatform(platform)}
+            key={platform.id}
+          >
+            {platform.name}
+          </MenuItem>
+        ))}
+      </MenuList>
     </Menu>
-  )
-}
+  );
+};
 
-export default PlatformSelector
+export default PlatformSelector;
--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -1,14 +1,26 @@
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import useData, { FetchRresponse } from "./useData";
+import apiClient from "../services/api-client";
+import platforms from "../data/platforms";
 
 interface Platform {
-    id: number;
-    name: string;
-    slug: string;
+  id: number;
+  name: string;
+  slug: string;
 }
 
-const usePlatform = () => useData<Platform>('/platforms/lists/parents')
+const usePlatform = () =>
+  useQuery({
+    queryKey: ["platform"],
+    queryFn: () =>
+      apiClient
+        .get<FetchRresponse<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    initialData: { count: platforms.length, results: platforms },
+  });
 
-export default usePlatform 
+export default usePlatform;
 
 // import platforms from "../data/platforms";
 
