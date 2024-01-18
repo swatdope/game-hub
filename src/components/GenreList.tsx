8c642e7e71d90b4bec5e0170181ebb0afeb8c8a5
Tenzin Ngudup <tenzinngodup900@gmail.com>
2024-01-18 19:06:35 +0530

    Fetching genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,31 +1,56 @@
-import useGenres, {Genre} from "../hooks/useGenres"
-import { Button, HStack, Heading, Image, List, ListItem, Spinner,  } from "@chakra-ui/react"
-import getCroppedImageUrl from "../services/image-url"
+import useGenres, { Genre } from "../hooks/useGenres";
+import {
+  Button,
+  HStack,
+  Heading,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+} from "@chakra-ui/react";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
-    onSelectGenre: (genre: Genre) => void;
-    selectedGenre: Genre | null;
-
+  onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({onSelectGenre, selectedGenre}: Props) => {
-    const {data,isLoading,error} = useGenres()
-    if(error) return null
-    if(isLoading) return <Spinner />    
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
+  const { data, isLoading, error } = useGenres();
+  if (error) return null;
+  if (isLoading) return <Spinner />;
   return (
     <>
-    <Heading fontSize='2xl' marginBottom={3} marginTop={10} > Genres </Heading>
-    <List>
-        {data.map(genre =>
-             <ListItem key={genre.id} paddingY='5px' >
+      <Heading fontSize="2xl" marginBottom={3} marginTop={10}>
+        {" "}
+        Genres{" "}
+      </Heading>
+      <List>
+        {data?.results.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
             <HStack>
-                <Image objectFit='cover' boxSize="32px" borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
-                <Button whiteSpace="normal" textAlign='left' fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectGenre(genre)} fontSize='lg' variant='link' >{genre.name}</Button>
+              <Image
+                objectFit="cover"
+                boxSize="32px"
+                borderRadius={8}
+                src={getCroppedImageUrl(genre.image_background)}
+              />
+              <Button
+                whiteSpace="normal"
+                textAlign="left"
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                onClick={() => onSelectGenre(genre)}
+                fontSize="lg"
+                variant="link"
+              >
+                {genre.name}
+              </Button>
             </HStack>
-        </ListItem> )}
-    </List>
+          </ListItem>
+        ))}
+      </List>
     </>
-  )
-}
+  );
+};
 
-export default GenreList
+export default GenreList;
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
-import App from './App.tsx'
-import theme from './theme.ts'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import App from "./App.tsx";
+import theme from "./theme.ts";
+import "./index.css";
 
+const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme} >
+    <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ChakraProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
